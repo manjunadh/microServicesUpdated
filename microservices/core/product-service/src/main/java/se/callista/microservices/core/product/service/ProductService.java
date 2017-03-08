d9438841ea1aff8016b391be06b064f0bc1fa308
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-08 12:49:50 +0530

    First Commit

--- microservices/core/product-service/src/main/java/se/callista/microservices/core/product/service/ProductService.java
+++ microservices/core/product-service/src/main/java/se/callista/microservices/core/product/service/ProductService.java
@@ -3,6 +3,8 @@ package se.callista.microservices.core.product.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -20,6 +22,7 @@ import se.callista.microservices.core.product.model.Product;
 @Service
 @Transactional
 public class ProductService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 	public static final String productName = "created";
 	public static final String RECIPIENT_QUEUE_NAME_SUFFIX = "recipient-";
 
@@ -43,6 +46,7 @@ public class ProductService {
 
 	@Cacheable(value = "allproductscache")
 	public List<Product> getAllProducts() {
+		LOGGER.info("fetching all products");
 		List<Product> products = new ArrayList<>();
 		productRepository.findAll().forEach(products::add);
 		return products;
@@ -50,21 +54,25 @@ public class ProductService {
 
 	@Cacheable(value = "products", key = "#id")
 	public Product getProduct(String id) {
+		LOGGER.info("fetching products for id" + id);
 		return productRepository.findOne(id);
 	}
 
 	@CachePut(value = "products", key = "#result.id")
 	public void addProduct(Product product) {
+		LOGGER.info("adding product to existing");
 		productRepository.save(product);
 	}
 
 	@CachePut(value = "products", key = "#product.id")
 	public void updateProduct(String id, Product product) {
+		LOGGER.info("updating product with id" + id);
 		productRepository.save(product);
 	}
 
 	@CacheEvict(value = "products", key = "#id")
 	public void deleteProduct(String id) {
+		LOGGER.info("deleting product with id"+id);
 		productRepository.delete(id);
 	}
 
